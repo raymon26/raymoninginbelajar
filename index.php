4b2d7533391461423fa92c492a8c22fc38cafd9d
raymon26 <113507099+raymon26@users.noreply.github.com>
2022-09-14 01:12:03 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+
+<html> <head>
+
+<title>Login Form</title> <link rel="stylesheet" href="style.css" type="text/css">
+
+</head>
+
+<body>
+
+<!-- <h2>Login Form </h2> -->
+
+<form action="tampil.php" method="POST">
+
+<h2>Form Login </h2> <div class="imgcontainer">
+
+<img src="icon.png" alt="Avatar" class="avatar">
+
+</div>
+
+<div class="container">
+
+<label><b>Username</b></label>
+
+Username"
+<input type="text" placeholder="Masukkan
+
+name="username"
+
+required>
+
+<label><b>Password</b></label> <input type="password"
+
+placeholder="Masukkan
+Password"
+
+name="email" required>
+
+<button type="submit">Masuk</button> <input type="checkbox" checked="checked"><span>Ingat Saya</span> </div>
+
+<div class="container" style="background-color: #f1f1f1"> <button type="button" class="cancelbtn">Batal</button> <span class="psw">Lupa <a href="#">password?</a></span> </div>
+
+</form>
+
+</body> </html>
